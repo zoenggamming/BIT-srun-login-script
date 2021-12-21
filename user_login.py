e033399ba0490c49d14ac92501769c6bf3c1af6f
zhangjinming <zhangjinming@senseauto.com>
2021-12-21 20:36:28 +0800

    code for secure user login

---
+++ user_login.py
@@ -0,0 +1,11 @@
+import os 
+import getpass
+
+def input_password():
+    passwd = getpass.getpass("Passord Plz~ : ")
+    return passwd
+
+def input_username():
+    username = input("username Plz~ : ")
+    return username
+
